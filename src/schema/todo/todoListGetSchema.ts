dd1fa754a85bc50bf277a799c3346fff2ec6c44f
haibin.zeng <haibin.zeng@coolkit.cn>
2022-11-07 09:43:24 +0800

    Todo 状态参数添加注释

--- src/schema/todo/todoListGetSchema.ts
+++ src/schema/todo/todoListGetSchema.ts
@@ -4,6 +4,7 @@ export default new Schema({
     keyword: {
         type: String
     },
+    // '0': 未完成，'1': 已完成
     status: {
         type: String,
         enum: ['0', '1']
--- src/schema/todo/todoPatchSchema.ts
+++ src/schema/todo/todoPatchSchema.ts
@@ -1,6 +1,7 @@
 import Schema from "validate"
 
 export default new Schema({
+    // '0': 未完成，'1': 已完成
     status: {
         required: true,
         type: String,
